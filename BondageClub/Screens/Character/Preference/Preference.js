d21f62589d328dd15529c563391e2602dda9b705
Wultir <62209995+wultir@users.noreply.github.com>
2020-12-13 13:35:07 +0100

    Add preferences for whispered emotes

--- BondageClub/Screens/Character/Preference/Preference.js
+++ BondageClub/Screens/Character/Preference/Preference.js
@@ -211,6 +211,7 @@ function PreferenceInit(C) {
 	if (C.ChatSettings.ShowAutomaticMessages == null) C.ChatSettings.ShowAutomaticMessages = false;
 	if (C.ChatSettings.WhiteSpace == null) C.ChatSettings.WhiteSpace = "Preserve";
 	if (C.ChatSettings.ColorActivities == null) C.ChatSettings.ColorActivities = true;
+	if (typeof C.ChatSettings.WhisperedEmotes !== "boolean") C.ChatSettings.WhisperedEmotes = false;
 	delete C.ChatSettings.AutoOOC;
 	delete C.ChatSettings.OOCWhispers;
 	if (!C.VisualSettings) C.VisualSettings = { ForceFullHeight: false };
@@ -766,6 +767,7 @@ function PreferenceSubscreenChatRun() {
 	DrawCheckbox(1200, 492, 64, 64, TextGet("ShowAutomaticMessages"), Player.ChatSettings.ShowAutomaticMessages);
 	DrawCheckbox(1200, 572, 64, 64, TextGet("PreserveWhitespace"), Player.ChatSettings.WhiteSpace == "Preserve");
 	DrawCheckbox(1200, 652, 64, 64, TextGet("ColorActivities"), Player.ChatSettings.ColorActivities);
+	DrawCheckbox(1200, 732, 64, 64, TextGet("WhisperedEmotes"), Player.ChatSettings.WhisperedEmotes);
 	MainCanvas.textAlign = "center";
 	DrawBackNextButton(1000, 190, 350, 70, TextGet(PreferenceChatColorThemeSelected), "White", "",
 		() => TextGet((PreferenceChatColorThemeIndex == 0) ? PreferenceChatColorThemeList[PreferenceChatColorThemeList.length - 1] : PreferenceChatColorThemeList[PreferenceChatColorThemeIndex - 1]),
@@ -1006,6 +1008,7 @@ function PreferenceSubscreenChatClick() {
 		if (MouseYIn(492, 64)) Player.ChatSettings.ShowAutomaticMessages = !Player.ChatSettings.ShowAutomaticMessages;
 		if (MouseYIn(572, 64)) Player.ChatSettings.WhiteSpace = Player.ChatSettings.WhiteSpace == "Preserve" ? "" : "Preserve";
 		if (MouseYIn(652, 64)) Player.ChatSettings.ColorActivities = !Player.ChatSettings.ColorActivities;
+		if (MouseYIn(732, 64)) Player.ChatSettings.WhisperedEmotes = !Player.ChatSettings.WhisperedEmotes;
 	}
 
 	// If the user used one of the BackNextButtons
